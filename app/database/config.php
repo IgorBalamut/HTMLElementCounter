0f7a83847770205f42591047a23359c828b01bf1
Igor Balamut <igorvbalamut@hotmail.com>
2019-01-20 22:22:50 +0200

    github first version

--- app/database/config.php
+++ app/database/config.php
@@ -1,12 +1,5 @@
  <?php 
-if($_SERVER['SERVER_ADDR']=="1.1.1.1"){
-	define('HOST','localhost');
-	define('DB_USER', 'project_user');
-	define('DB_PASSWORD','project1');
-	define('DB_NAME','project');
-} else {
-	define('HOST','localhost');
-	define('DB_USER', 'project_user');
-	define('DB_PASSWORD','House123!');
-	define('DB_NAME','project');
-}
+define('HOST','localhost');
+define('DB_USER', 'project_user');
+define('DB_PASSWORD','project1');
+define('DB_NAME','project');
