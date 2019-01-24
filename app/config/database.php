adb29c47976445174446e9d4bdfc6702065a97be
Igor Balamut <igorvbalamut@hotmail.com>
2019-01-24 20:10:58 +0200

    prod ver 1.1.3

--- app/config/database.php
+++ app/config/database.php
@@ -4,7 +4,7 @@
 define('HOST', 'localhost');
 define('DB_USER', 'project_user');
 define('DB_NAME', 'project');
-define('DB_PASSWORD', 'project1');
+define('DB_PASSWORD', 'House123!');
 
 // database check period
 define('CHECK_TIME', 5);
